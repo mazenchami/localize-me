337b3e89087cb4f9ce75851bf7f7655c8ba75298
Mazen Chami <mazenchami@gmail.com>
2024-08-22 13:06:41 -0400

    apply rtl textinput considerations

--- app/(tabs)/(explore)/showcase.tsx
+++ app/(tabs)/(explore)/showcase.tsx
@@ -1,15 +1,26 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, TextInput } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { useTranslation } from "react-i18next";
+import { isRTL } from "@/i18n/i18next";
+import { useState } from "react";
 
 export default function TabTwoScreen() {
+  const [text, onChangeText] = useState("");
   const { t } = useTranslation(["showcaseScreen"]);
   return (
     <ThemedView style={styles.container}>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">{t("title")}</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <TextInput
+          placeholder={t("placeholder")}
+          style={[styles.input, styles.rtlInput]}
+          onChangeText={onChangeText}
+          value={text}
+        />
       </ThemedView>
     </ThemedView>
   );
@@ -26,4 +37,15 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     gap: 8,
   },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  rtlInput: isRTL ? { textAlign: "right" } : {},
 });
